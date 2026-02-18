90ba5068c677bde117309ee2bafbd513f419923b
Daniela <tdaniela210921@gmail.com>
2026-02-18 12:06:21 +0200

    componenta documents

---
+++ frontend/src/components/documents/index.ts
@@ -0,0 +1,6 @@
+export { DocumentFilters } from './DocumentFilters';
+export { DocumentTable } from './DocumentTable';
+export { DocumentRow } from './DocumentRow';
+export { DocumentEmptyState } from './DocumentEmptyState';
+export { UploadDialog } from './UploadDialog';
+export { DeleteConfirmDialog } from './DeleteConfirmDialog';
