d5a752a7141820481fea1df5591c28e226859b59
a <a>
2026-03-03 21:29:50 +0200

    adaugare componente UI pentru gestionare erori si notificari

---
+++ frontend/src/components/ui/sonner.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Toaster as Sonner } from "sonner"
+
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+export function Toaster(props: ToasterProps) {
+    return (
+        <Sonner
+            theme="system"
+            className="toaster group"
+            {...props}
+        />
+    )
+}
